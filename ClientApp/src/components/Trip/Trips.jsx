d054e62c72977fd355e97f6d9885ea7f80e7246b
Robert Hardy <helloarchy@gmail.com>
2021-11-21 18:41:28 +0000

    Various updates * Add prettier, eslint, to enforce code style * Update home to detail app usage * Add Axios, fetch data from backend * Display actual data in trips table * Format date locally if date provided

--- ClientApp/src/components/Trip/Trips.jsx
+++ ClientApp/src/components/Trip/Trips.jsx
@@ -1,59 +1,75 @@
-﻿import React, {Component} from "react";
+﻿import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Trips extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            trips: [],
-            loading: false
-        }
-    }
+    this.state = {
+      trips: [],
+      loading: true
+    };
+  }
 
-    renderAllTripsTable(trips) {
-        return (
-            <table className={"table table-striped"}>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Date Started</th>
-                    <th>Date Completed</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>A</td>
-                    <td>B</td>
-                    <td>C</td>
-                    <td>D</td>
-                    <td>E</td>
-                </tr>
-                <tr>
-                    <td>A</td>
-                    <td>B</td>
-                    <td>C</td>
-                    <td>D</td>
-                    <td>E</td>
-                </tr>
-                </tbody>
-            </table>
-        )
-    }
-    
-    render() {
-        let content = this.state.loading ? (
-            <p><em>Loading...</em></p>
-        ) : (
-            this.renderAllTripsTable(this.state.trips)
-        )
-        return (
-          <div>
-              <h1>All trips</h1>
-              <p>Here you can see all trips</p>
-              {content}
-          </div>  
-        );
-    }
+  componentDidMount() {
+    this.populateTripsData();
+  }
+
+  populateTripsData() {
+    axios.get('api/Trips/GetTrips').then((result) => {
+      const response = result.data;
+      this.setState({
+        trips: response,
+        loading: false
+      });
+    });
+  }
+
+  renderAllTripsTable(trips) {
+    return (
+      <table className={'table table-striped'}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Date Started</th>
+            <th>Date Completed</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trips.map((trip) => {
+            return (
+              <tr key={trip.id}>
+                <td>{trip.name}</td>
+                <td>{trip.description}</td>
+                <td>{new Date(trip.dateStarted).toLocaleDateString()}</td>
+                <td>
+                  {trip.dateCompleted ? new Date(trip.dateCompleted).toLocaleDateString() : ' - '}
+                </td>
+                <td> - </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let content = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      this.renderAllTripsTable(this.state.trips)
+    );
+    return (
+      <div>
+        <h1>All trips</h1>
+        <p>Here you can see all trips</p>
+        {content}
+      </div>
+    );
+  }
 }
