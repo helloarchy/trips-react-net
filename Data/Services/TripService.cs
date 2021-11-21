a6c31cffd926d01423bf71dfec4ec0e55453a966
Robert Hardy <helloarchy@gmail.com>
2021-11-21 13:23:20 +0000

    Add service and seed data held in memory.

---
+++ Data/Services/TripService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Trips.Data.Models;
+
+namespace Trips.Data.Services
+{
+    public class TripService : ITripService
+    {
+        public List<Trip> GetAllTrips()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Trip GetTripById(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTrip(int tripId, Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteTrip(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddTrip(Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
