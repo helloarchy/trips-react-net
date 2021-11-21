8c12a137bbc554785ece7a8a185096e1c0012191
Robert Hardy <helloarchy@gmail.com>
2021-11-21 19:51:16 +0000

    Create a new trip via GUI

--- ClientApp/src/components/Trip/Create.jsx
+++ ClientApp/src/components/Trip/Create.jsx
@@ -1,8 +1,15 @@
 ﻿import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Create extends Component {
   constructor(props) {
     super(props);
+
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDateStarted = this.onChangeDateStarted.bind(this);
+    this.onChangeDateCompleted = this.onChangeDateCompleted.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       name: '',
@@ -12,6 +19,47 @@ export class Create extends Component {
     };
   }
 
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDateStarted(e) {
+    this.setState({
+      dateStarted: e.target.value
+    });
+  }
+
+  onChangeDateCompleted(e) {
+    this.setState({
+      dateCompleted: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const { history } = this.props;
+
+    let tripObject = {
+      Id: Math.floor(Math.random() * 1000),
+      Name: this.state.name,
+      Description: this.state.description,
+      DateStarted: this.state.dateStarted,
+      DateCompleted: this.state.dateCompleted
+    };
+
+    axios.post('api/Trips/AddTrip', tripObject).then((result) => {
+      history.push('/trips');
+    });
+  }
+
   render() {
     return (
       <div className="trip-form">
@@ -19,23 +67,45 @@ export class Create extends Component {
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Trip name: </label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.name}
+              onChange={this.onChangeName}
+            />
           </div>
+
           <div className="form-group">
             <label>Trip description: </label>
-            <textarea type="text" className="form-control" />
+            <textarea
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
           </div>
+
           <div className="row">
             <div className="col col-md-6 col-sm-6 col-xs-12">
               <div className="form-group">
                 <label>Date of start: </label>
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  value={this.state.dateStarted}
+                  onChange={this.onChangeDateStarted}
+                />
               </div>
             </div>
+
             <div className="col col-md-6 col-sm-6 col-xs-12">
               <div className="form-group">
                 <label>Date of completion: </label>
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  value={this.state.dateCompleted}
+                  onChange={this.onChangeDateCompleted}
+                />
               </div>
             </div>
           </div>
