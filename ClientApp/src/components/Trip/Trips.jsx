a87a35b56c1f62fcae199cea0d4b38f128b65ff8
Robert Hardy <helloarchy@gmail.com>
2021-11-21 18:10:21 +0000

    Add trips table with state

---
+++ ClientApp/src/components/Trip/Trips.jsx
@@ -0,0 +1,59 @@
+﻿import React, {Component} from "react";
+
+export class Trips extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            trips: [],
+            loading: false
+        }
+    }
+
+    renderAllTripsTable(trips) {
+        return (
+            <table className={"table table-striped"}>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Date Started</th>
+                    <th>Date Completed</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>A</td>
+                    <td>B</td>
+                    <td>C</td>
+                    <td>D</td>
+                    <td>E</td>
+                </tr>
+                <tr>
+                    <td>A</td>
+                    <td>B</td>
+                    <td>C</td>
+                    <td>D</td>
+                    <td>E</td>
+                </tr>
+                </tbody>
+            </table>
+        )
+    }
+    
+    render() {
+        let content = this.state.loading ? (
+            <p><em>Loading...</em></p>
+        ) : (
+            this.renderAllTripsTable(this.state.trips)
+        )
+        return (
+          <div>
+              <h1>All trips</h1>
+              <p>Here you can see all trips</p>
+              {content}
+          </div>  
+        );
+    }
+}
