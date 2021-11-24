6a50c32b6e8abe252ef01a9817168c20ae5ce2eb
Robert Hardy <helloarchy@gmail.com>
2021-11-24 20:43:56 +0000

    Add redux

---
+++ ClientApp/src/actions/tripActions.js
@@ -0,0 +1,10 @@
+﻿import axios from 'axios';
+
+export const GET_ALL_TRIPS_REQUEST = 'GET_ALL_TRIPS_REQUEST';
+export const GET_ALL_TRIPS_SUCCESS = 'GET_ALL_TRIPS_SUCCESS';
+export const GET_ALL_TRIPS_ERROR = 'GET_ALL_TRIPS_ERROR';
+
+const getTripsSuccess = (payload) => ({
+  type: GET_ALL_TRIPS_SUCCESS,
+  payload
+});
